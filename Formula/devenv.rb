0faf618c7daed8d3b8296e494a6add16904d7a87
Arash Rasoulzadeh <a.rasoulzadeh@snappgrocery.com>
2026-04-28 20:49:05 +0330

    update to commit-a4df07a

--- Formula/devenv.rb
+++ Formula/devenv.rb
@@ -1,11 +1,11 @@
 class Devenv < Formula
   desc "Dev environment tool"
   homepage "https://github.com/arashrasoulzadeh/devenv"
-  version "commit-b2a0221"
+  version "1.0"
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/arashrasoulzadeh/devenv/releases/download/commit-b2a0221/devenv-macos-arm64"
-    sha256 "3eed0f1f2bcc88b691df5d02aedbf983802f1732a4da547d233bcf41fdd6693d"
+    url "https://github.com/arashrasoulzadeh/devenv/releases/download/commit-8ffa938/devenv-macos-arm64"
+    sha256 "d344d139ad2f46fbeedf55dc73f45d33b1cfc7923fa5d81f3a87e5519661f9ab"
   end
 
   def install
