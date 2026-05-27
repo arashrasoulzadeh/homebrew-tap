f05a2d07256e13bb31059fa1f5e208d87fa7132f
Arash Rasoulzadeh <a.rasoulzadeh@snappgrocery.com>
2026-05-28 00:56:12 +0330

    update to commit-8ff33a0

--- Formula/devenv.rb
+++ Formula/devenv.rb
@@ -1,7 +1,7 @@
 class Devenv < Formula
   desc "Dev environment tool"
   homepage "https://github.com/arashrasoulzadeh/devenv"
-  version "1.1"
+  version "1.2"
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/arashrasoulzadeh/devenv/releases/download/commit-8ff33a0/devenv-macos-arm64"
